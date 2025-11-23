6732a8595fb6ab981a6b05a064ead18d3887f95f
Maxime Grauliere <ton-email-github@exemple.com>
2025-11-22 20:16:38 -0500

    Isoler les pages marketing du shell cockpit + reset scroll

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,27 +1,34 @@
-﻿import "@/styles/globals.css";
-import Head from "next/head";
+﻿import "../styles/globals.css";
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
 import ShellLayout from "@/components/ShellLayout";
 
 function MyApp({ Component, pageProps }) {
-  const getLayout =
-    Component.getLayout ||
-    ((page) => (
-      <ShellLayout>{page}</ShellLayout>
-    ));
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = () => {
+      if (typeof window !== "undefined") {
+        window.scrollTo(0, 0);
+      }
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => router.events.off("routeChangeComplete", handleRouteChange);
+  }, [router.events]);
+
+  const marketingRoutes = ["/", "/managers"];
+  const isMarketing = marketingRoutes.includes(router.pathname);
+
+  if (isMarketing) {
+    return <Component {...pageProps} />;
+  }
 
   return (
-    <>
-      <Head>
-        <title>Sync GPT Hub</title>
-        <meta
-          name="description"
-          content="Hub IA de vente pour Sync Productions – prospection, rédaction, analyse et radar opportunités."
-        />
-      </Head>
-      {getLayout(<Component {...pageProps} />)}
-    </>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
 
 export default MyApp;
-
