1f070dcbf595d6eaf5c2ed010d65ddeda3c31d52
Maxime Grauliere <ton-email-github@exemple.com>
2025-11-22 20:18:56 -0500

    Simplifier _app.js (sans Layout) pour cockpit + pages marketing

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,6 @@
 ﻿import "../styles/globals.css";
 import React, { useEffect } from "react";
 import { useRouter } from "next/router";
-import ShellLayout from "@/components/ShellLayout";
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
@@ -17,18 +16,7 @@ function MyApp({ Component, pageProps }) {
     return () => router.events.off("routeChangeComplete", handleRouteChange);
   }, [router.events]);
 
-  const marketingRoutes = ["/", "/managers"];
-  const isMarketing = marketingRoutes.includes(router.pathname);
-
-  if (isMarketing) {
-    return <Component {...pageProps} />;
-  }
-
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
